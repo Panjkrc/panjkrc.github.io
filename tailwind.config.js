ebae65d6377765c329085b2a9a0561b5c293169e
Panjkrc <panjkrc.mihael@gmail.com>
2023-06-27 17:56:01 +0200

    Clean up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['*.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-		require('@tailwindcss/typography'),
-	],
+	content: ['*.html'],
 }
-
